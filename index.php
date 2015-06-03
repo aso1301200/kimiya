36eba9988fa6950e992604310fcabcdda48f6a8d
紗代 <@fukufuku>
2015-06-03 11:44:53 +0900

    6月3日

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<meta http-equiv="Content-Style-Type" content="text/css">
+
+<title>きみ屋</title>
+
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<div id="page">
+
+	<div id="masthead" role="banner">
+
+		<div id="header-inner">
+
+			<div id="header-title-area">
+
+				<h1 class="site-title-img"><a href="index.php" title="ギャラリー（教育・スクール：ブルー）" rel="home"><img src="images/sample_logo_01.png" alt="ギャラリー（教育・スクール：ブルー）"></a></h1>
+
+			</div><!-- #header-title-area -->
+
+			<div id="header-widget-area">
+
+				<form role="search" method="get" id="searchform" class="searchform" action="*******">
+					<div>
+						<input type="text" value="" name="s" id="s">
+						<input type="submit" id="searchsubmit" value="検索">
+					</div>					</form>
+
+			</div><!-- #header-widget-area -->
+
+			<div class="clear"></div>
+
+		</div><!-- .header-inner -->
+
+	</div><!-- #masthead -->
+</div>
+</body>
+</html>
