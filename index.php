b057228005ea95ebcb476b82f27681b34f305696
公也 <@fpc>
2015-06-07 17:03:33 +0900

    最終コミット(2015/06/07)

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang=''>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <meta http-equiv="Content-Style-Type" content="text/css">
 
-	<!-- タグ関係（MenuMaker） 2015/06/07 -->
-   <meta charset='utf-8'>
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="styles.css">
-   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-   <script src="script.js"></script>
-	<!-- ここまで、タグ関係 -->
+<!-- タグ関係（MenuMaker） 2015/06/07 -->
+<meta charset='utf-8'>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="stylesheet" href="styles.css" type="text/css">
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="script.js"></script>
+<!-- ここまで、タグ関係 -->
 <title>きみ屋</title>
 
-<link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
 <div id="page">
