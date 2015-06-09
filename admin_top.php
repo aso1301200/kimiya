11952e5f1c96bd9e668e260d4e3a5a70948c8096
紗代 <@fukufuku>
2015-06-09 14:36:13 +0900

    管理者画面　作成

--- admin_top.php
+++ admin_top.php
@@ -10,12 +10,12 @@
 </head>
 
 <body>
-	<div id="">
+	<div id="admin">
 		<h1>きみ屋　商品情報</h1>
 
 		<div class="post">
-			<h4><a href="#">商品追加</a></h4>
-				<img src="images/sample_logo_01.png" alt="" />
+			<h3>商品追加</h3>
+				<img src="images/sample_logo_01.png" alt=""  id="admin_image"/>
 				<p>
 					<form action="" method="post">
 					<table border="0">
@@ -61,16 +61,31 @@
 					  	</td>
 					  </tr>
 					  <tr>
-					    <td></td>
-					    <td>
-					      <input type="submit" value="送信">
-					      <input type="reset" value="リセット">
-					    </td>
+					  	<td align="right"><b>タイプ：</b></td>
+					  	<td>
+					  		<select>
+								<option value="1">おひつじ座</option>
+								<option value="2">おうし座</option>
+								<option value="3">ふたご座</option>
+								<option value="4">かに座</option>
+								<option value="5">しし座</option>
+								<option value="6">おとめ座</option>
+								<option value="7">てんびん座</option>
+								<option value="8">さそり座</option>
+					  		</select>
+					  	</td>
+					  </tr>
+					  <tr>
+					  	<td align="right"><b>写真：</b></td>
+					  	<td><input type="file" name="photo"></td>
 					  </tr>
 					</table>
 					</form>
 				</p>
-				<p><a class="more" href="#">設定 »</a></p>
+				<p>
+					<input class="more" type="reset" value="リセット">
+					<input class="more" type="submit" value="追加">
+				</p>
 		</div>
 
 		<div class="post">
